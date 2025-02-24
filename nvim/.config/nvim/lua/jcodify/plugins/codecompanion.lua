cbf7fa7a4df61033054a7845b537f9e6aa394f7f
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-02-24 05:38:54 -0800

    Add Ollama DeepSeek-R1 adapter configurationand update nvim-treesitter dependency

---
+++ nvim/.config/nvim/lua/jcodify/plugins/codecompanion.lua
@@ -0,0 +1,28 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+
+	keys = {
+		{ "<leader>ac", "<cmd>CodeCompanionChat<CR>", desc = "Code companion chat" },
+	},
+	opts = {
+		strategies = {
+			chat = { adapter = "ollama" },
+			inline = { adapter = "ollama" },
+		},
+		adapters = {
+			ollama_deepseek_r1 = function()
+				return require("codecompanion.adapters").extend("ollama", {
+					schema = {
+						model = {
+							default = "deepseek-r1:latest",
+						},
+					},
+				})
+			end,
+		},
+	},
+}
