9d4627d3e679d8e82f3694d07ccb06c7e4d95e7d
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-05-03 07:10:34 -0700

    update nvim packages

---
+++ nvim/.config/nvim/lua/jcodify/plugins/venv-selector.lua
@@ -0,0 +1,19 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	---@type venv-selector.Config
+	opts = {
+		-- Your settings go here
+	},
+	keys = {
+		-- Keymap to open VenvSelector to pick a venv.
+		{ "<leader>lpvs", "<cmd>VenvSelect<cr>" },
+	},
+}
