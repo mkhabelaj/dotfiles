f3a24c32b8950be39ab88bd5e8675d87a77696b3
Jackson Mkhabela <jacksonmkhabela@Jacksons-MacBook-Pro.local>
2025-05-19 05:10:10 -0700

    Add Vue and ts ls server

--- nvim/.config/nvim/lua/jcodify/plugins/lsp/lsp-configuration.lua
+++ nvim/.config/nvim/lua/jcodify/plugins/lsp/lsp-configuration.lua
@@ -29,7 +29,7 @@ return {
 			-- 2) LSP servers
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"tsserver",
+					"ts_ls",
 					"html",
 					"cssls",
 					"tailwindcss",
@@ -102,12 +102,70 @@ return {
 					lspconfig[server_name].setup({ capabilities = caps })
 				end,
 
+				["ts_ls"] = function()
+					lspconfig.ts_ls.setup({
+						capabilities = caps,
+						init_options = {
+							plugins = {
+								{
+									name = "@vue/typescript-plugin",
+									location = vim.fn.stdpath("data")
+										.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+									languages = { "vue" },
+								},
+							},
+						},
+						settings = {
+							typescript = {
+								tsserver = {
+									useSyntaxServer = false,
+								},
+								inlayHints = {
+									includeInlayParameterNameHints = "all",
+									includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+									includeInlayFunctionParameterTypeHints = true,
+									includeInlayVariableTypeHints = true,
+									includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+									includeInlayPropertyDeclarationTypeHints = true,
+									includeInlayFunctionLikeReturnTypeHints = true,
+									includeInlayEnumMemberValueHints = true,
+								},
+							},
+						},
+						filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+					})
+				end,
 				-- volar (Vue)
 				["volar"] = function()
 					lspconfig.volar.setup({
 						capabilities = caps,
-						filetypes = { "vue", "typescript", "javascript", "javascriptreact", "typescriptreact" },
-						init_options = { vue = { hybridMode = true } },
+						init_options = {
+							vue = {
+								hybridMode = false,
+							},
+						},
+						settings = {
+							typescript = {
+								inlayHints = {
+									enumMemberValues = {
+										enabled = true,
+									},
+									functionLikeReturnTypes = {
+										enabled = true,
+									},
+									propertyDeclarationTypes = {
+										enabled = true,
+									},
+									parameterTypes = {
+										enabled = true,
+										suppressWhenArgumentMatchesName = true,
+									},
+									variableTypes = {
+										enabled = true,
+									},
+								},
+							},
+						},
 					})
 				end,
 
