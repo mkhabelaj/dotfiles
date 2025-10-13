48947b3c0a3130a475eb9f977db0c161a49541c0
Jackson Mkhabela <jmkhabela@gmail.com>
2025-10-13 07:35:50 -0700

    add diff and blame

--- nvim/.config/nvim/lua/jcodify/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/jcodify/plugins/gitsigns.lua
@@ -1,4 +1,20 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>ga",
+			function()
+				require("gitsigns").blame()
+			end,
+			desc = "Toggle Line Blame (gitsigns)",
+		},
+		{
+			"<leader>gD",
+			function()
+				require("gitsigns").diffthis()
+			end,
+			desc = "Git Diff(gitsigns)",
+		},
+	},
 }
