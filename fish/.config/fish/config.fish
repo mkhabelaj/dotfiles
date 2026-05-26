145a1da1917f9e64677a8d7fe6ffa5dbf3c5b1d4
= <=>
2026-05-26 14:18:06 -0700

    Add git abbr's

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,15 @@
 set -gx TMUX_CONFIG ~/.config/tmux/tmux.conf
 alias s='sesh connect (sesh list -i | gum filter --limit 1 --placeholder "Pick a sesh" --prompt="⚡")'
 alias sg='sesh connect (sesh list -i | gum filter --limit 1 --placeholder "Pick a sesh" --prompt="⚡")'
+
+# Git checkout
+abbr -a gco git checkout
+
+# Common additions
+abbr -a g git 
+abbr -a ga git add
+abbr -a gc git commit
+abbr -a gst git status
+abbr -a gpl git pull
+abbr -a gps git push
+
