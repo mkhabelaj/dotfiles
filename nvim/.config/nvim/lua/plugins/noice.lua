c2d77c146e73bfb07c89aec2f64e17f5f9a2badf
mkhabelaj <jmkhabela@gmail.com>
2026-05-26 16:11:40 -0700

    Fix vim.notify conflict between Noice and Snacks notifier
    Disable Noice's notify handler so Snacks notifier owns vim.notify.
    Snacks loads first (priority=1000) and is used for notification history
    via <leader>n, so Noice should not compete for vim.notify.

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -20,7 +20,8 @@ return {
 				filter = { event = "notify", find = "No information available" },
 				opts = { skip = true },
 			},
-			presets = { lsp_doc_border = true },
+			notify = { enabled = false },
+		presets = { lsp_doc_border = true },
 
 			lsp = {
 				progress = {
