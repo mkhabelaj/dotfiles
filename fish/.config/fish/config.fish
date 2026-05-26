1e104346d91cc7f4f7eddb37a02ffee6d6fefbcc
mkhabelaj <jmkhabela@gmail.com>
2026-05-26 06:14:37 -0700

    add alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,5 @@
 #end
 ~/.local/bin/mise activate fish | source
 set -gx TMUX_CONFIG ~/.config/tmux/tmux.conf
+alias s='sesh connect (sesh list -i | gum filter --limit 1 --placeholder "Pick a sesh" --prompt="⚡")'
+alias sg='sesh connect (sesh list -i | gum filter --limit 1 --placeholder "Pick a sesh" --prompt="⚡")'
