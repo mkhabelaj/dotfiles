265b29f6cd5d25c086c893ebaa9526a47c2883c3
= <=>
2026-06-04 07:55:48 -0700

    Add diagnostic

---
+++ nvim/.config/nvim/lua/config/core/diagnostics.lua
@@ -0,0 +1,10 @@
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+		},
+	},
+})
